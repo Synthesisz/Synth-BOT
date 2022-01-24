3373864f4f9e1599cd79f9cb8bcc0c874273e645
Synthesisz <84045671+Synthesisz@users.noreply.github.com>
2022-01-24 05:05:40 +0200

    Add files via upload

--- utility/fetch-quote.js
+++ utility/fetch-quote.js
@@ -4,7 +4,5 @@ module.exports.getQuote = async endpoint => {
   const data = await fetch(`https://animechan.vercel.app/api/${endpoint}`);
   const response = await data.json();
 
-  return `**Anime:** ${response.anime}
-**Character:** ${response.character}
-**Quote:** ${response.quote}`;
+  return response;
 };
