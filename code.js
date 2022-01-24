2afa3f51255c337e5c775c9c66b88aa556cbc7a3
Synthesisz <84045671+Synthesisz@users.noreply.github.com>
2022-01-24 05:04:36 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,58 +1,16 @@
-// Require the necessary discord.js classes
-const {Client, Collection, Intents} = require('discord.js');
+const {Client, Collection} = require('discord.js');
 const {token} = require('./config.json');
-const fs = require('fs');
-//const commandHandler = require("./commands");
-// Create a new client instance
-const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  ],
-});
-
+const client = new Client({intents: 14023});
+//DISTUBE
+const {DisTube} = require('distube');
+//const {SpotifyPlugin} = require('@distube/spotify');
+require('./Handlers/eventhandler')(client);
+require('./Handlers/commandhandler')(client);
 client.commands = new Collection();
-const commandFiles = fs
-  .readdirSync('./commands')
-  .filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  // Set a new item in the Collection
-  // With the key as the command name and the value as the exported module
-  client.commands.set(command.data.name, command);
-}
-// client.once('ready', () => {
-//   console.log('Ready!');
-// });
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: 'There was an error while executing this command!',
-      ephemeral: true,
-    });
-  }
+client.distube = new DisTube(client, {
+  emitAddSongWhenCreatingQueue: false,
+  // plugins: [new SpotifyPlugin()],
 });
-const eventFiles = fs
-  .readdirSync('./events')
-  .filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
-
+module.exports = client; //END OF DISTUBE
 client.login(token);
